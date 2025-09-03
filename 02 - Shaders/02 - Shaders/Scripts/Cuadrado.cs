69e5162e30396f6b8bf344cf4f58d959b7835d8a
Jhonny Felipez Andrade <93157310+jrfelipez@users.noreply.github.com>
2025-09-03 19:24:00 -0400

    Agregando Shaders

---
+++ 02 - Shaders/02 - Shaders/Scripts/Cuadrado.cs
@@ -0,0 +1,68 @@
+// Cuadrado de color rojo
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuadrado : MonoBehaviour
+{
+    private Vector3[] vertices;
+    private int[] indices;
+    private GameObject objetoCuadrado;
+    private GameObject miCamara;
+
+    void Start()
+    {
+        objetoCuadrado = new GameObject();
+        objetoCuadrado.AddComponent<MeshFilter>();
+        objetoCuadrado.GetComponent<MeshFilter>().mesh = new Mesh();
+        objetoCuadrado.AddComponent<MeshRenderer>();
+        CreaModelo();
+        UpdateMalla();
+        CreaMaterial();
+        CreaCamara();
+    }
+
+    private void CreaMaterial()
+    {
+        Material nuevoMaterial = new Material(Shader.Find("ShaderBasico"));
+        objetoCuadrado.GetComponent<MeshRenderer>().material = nuevoMaterial;
+    }
+
+    private void CreaCamara()
+    {
+        miCamara = new GameObject();
+        miCamara.AddComponent<Camera>();
+        miCamara.transform.position = new Vector3(0, 2, 0);
+        miCamara.transform.rotation = Quaternion.Euler(90, 0, 0);
+        miCamara.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
+        miCamara.GetComponent<Camera>().backgroundColor = Color.black;
+    }
+
+    private void CreaModelo()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0), // 0
+            new Vector3(1,0,0), // 1
+            new Vector3(1,0,1), // 2
+            new Vector3(0,0,1) // 3
+        };
+        
+        indices = new int[]
+        {
+            0,2,1, // T0
+            0,3,2  // T1
+        };
+    }
+
+    private void UpdateMalla()
+    {
+        objetoCuadrado.GetComponent<MeshFilter>().mesh.vertices = vertices;
+        objetoCuadrado.GetComponent<MeshFilter>().mesh.triangles = indices;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ 02 - Shaders/02 - Shaders/Shaders/ShaderBasico.shader
@@ -0,0 +1,34 @@
+Shader "ShaderBasico"
+{
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            struct appdata
+            {
+                float4 vertex: POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex: SV_POSITION;
+            };
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            fixed4 frag(v2f i): SV_Target
+            {
+                return half4(1.0f, 0.0f, 0.0f, 1.0f);
+            }
+            ENDCG
+        }
+    }
+}
